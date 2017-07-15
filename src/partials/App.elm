c2a139367d7723addb53e5a7f115241f072f2e97
Rakesh Chatrath <rakesh@rakeshchatrath.me>
2017-07-14 22:20:26 -0500

    Add class logic to link and header

--- src/partials/App.elm
+++ src/partials/App.elm
@@ -2,7 +2,7 @@ module App exposing (..)
 
 import Html exposing (Html, text, div, img, h1, a, p)
 import Html.Attributes exposing (src, classList)
-import Html.Events exposing (onMouseOver, onMouseOut)
+import Html.Events exposing (onMouseOver, onMouseOut, onMouseEnter)
 
 
 ---- MODEL ----
@@ -12,28 +12,52 @@ type alias Model =
     { title : String
     , icon : String
     , description : String
+    , isProgramming : Bool
+    , isPhotography : Bool
+    , isBlog: Bool
+    , isResume : Bool
     }
 
 components =
   { programming = { title = "Programming"
                   , icon = "./static/img/programming.svg"
                   , description = "I do full stack web development with interests in data science, machine learning, computer vision, and computational biology research."
+                  , isProgramming = True
+                  , isPhotography = False
+                  , isBlog = False
+                  , isResume = False
                   }
     , photography = { title = "Photography"
                     , icon = "./static/img/photography.svg"
                     , description = "I love taking photos of things - especially street, landscape and portraiture photography."
+                    , isProgramming = False
+                    , isPhotography = True
+                    , isBlog = False
+                    , isResume = False
                     }
     , blog = { title = "Blog"
              , icon = "./static/img/blog.svg"
              , description = "I like to write about all sorts of things pertaining to myself and technology."
+             , isProgramming = False
+             , isPhotography = False
+             , isBlog = True
+             , isResume = False
              }
     , resume = { title = "Resume"
                , icon = "./static/img/resume.svg"
                , description = "Interested in hiring me? Contact me at rakesh@rakeshchatrath.me."
+               , isProgramming = False
+               , isPhotography = False
+               , isBlog = False
+               , isResume = True
                }
     , default = { title = "Hi. I'm Rakesh Chatrath."
                 , icon = "./static/img/logo.svg"
                 , description = "I like all things technology, photography and biology."
+                , isProgramming = False
+                , isPhotography = False
+                , isBlog = False
+                , isResume = False
                 }
   }
 
@@ -43,6 +67,10 @@ init path =
     ( { title = components.default.title
       , icon = path
       , description = components.default.description
+      , isProgramming = components.default.isProgramming
+      , isPhotography = components.default.isPhotography
+      , isBlog = components.default.isBlog
+      , isResume = components.default.isResume
       }
     , Cmd.none
     )
@@ -51,13 +79,6 @@ init path =
 
 ---- UPDATE ----
 
-
--- type Msg
---     = NoOp
---
--- update : Msg -> Model -> ( Model, Cmd Msg )
--- update msg model =
---     ( model, Cmd.none )
 
 type Msg
   = Programming
@@ -74,6 +95,10 @@ update msg model =
         | title = components.programming.title
         , description = components.programming.description
         , icon = components.programming.icon
+        , isProgramming = components.programming.isProgramming
+        , isPhotography = components.programming.isPhotography
+        , isBlog = components.programming.isBlog
+        , isResume = components.programming.isResume
         }
         , Cmd.none )
     Photography ->
@@ -81,6 +106,10 @@ update msg model =
         | title = components.photography.title
         , description = components.photography.description
         , icon = components.photography.icon
+        , isProgramming = components.photography.isProgramming
+        , isPhotography = components.photography.isPhotography
+        , isBlog = components.photography.isBlog
+        , isResume = components.photography.isResume
         }
       , Cmd.none )
     Blog ->
@@ -88,6 +117,10 @@ update msg model =
         | title = components.blog.title
         , description = components.blog.description
         , icon = components.blog.icon
+        , isProgramming = components.blog.isProgramming
+        , isPhotography = components.blog.isPhotography
+        , isBlog = components.blog.isBlog
+        , isResume = components.blog.isResume
         }
       , Cmd.none )
     Resume ->
@@ -95,6 +128,10 @@ update msg model =
         | title = components.resume.title
         , description = components.resume.description
         , icon = components.resume.icon
+        , isProgramming = components.resume.isProgramming
+        , isPhotography = components.resume.isPhotography
+        , isBlog = components.resume.isBlog
+        , isResume = components.resume.isResume
         }
       , Cmd.none )
     Default ->
@@ -102,6 +139,10 @@ update msg model =
         | title = components.default.title
         , description = components.default.description
         , icon = components.default.icon
+        , isProgramming = components.default.isProgramming
+        , isPhotography = components.default.isPhotography
+        , isBlog = components.default.isBlog
+        , isResume = components.default.isResume
         }
       , Cmd.none )
 
@@ -116,15 +157,49 @@ view model =
               [ div [ classList [ ("image-container", True) ] ] [ img [ src model.icon ] [] ]
               , div [ classList [ ("header-container", True ) ] ]
                     [
-                      h1 [ classList [ ("home-title-header", True) ] ] [ text model.title ]
-                    , p [ classList [ ("home-title-description", True) ] ] [ text model.description ]
+                      h1 [ classList [ ("home-title-header", True)
+                                     , ("programming", model.isProgramming)
+                                     , ("photography", model.isPhotography)
+                                     , ("blog", model.isBlog)
+                                     , ("resume", model.isResume)
+                                     ]
+                         ]
+                         [ text model.title ]
+                    , p [ classList [ ("home-title-description", True)
+                                    , ("programming", model.isProgramming)
+                                    , ("photography", model.isPhotography)
+                                    , ("blog", model.isBlog)
+                                    , ("resume", model.isResume)
+                                    ]
+                        ]
+                        [ text model.description ]
                     ]
               ]
         , div [ classList [ ("links-container", True ) ] ]
-              [ p [] [ a [ onMouseOver Programming, onMouseOut Default ] [ text "Programming" ] ]
-              , p [] [ a [ onMouseOver Photography, onMouseOut Default ] [ text "Photography" ] ]
-              , p [] [ a [ onMouseOver Blog, onMouseOut Default ] [ text "Blog" ] ]
-              , p [] [ a [ onMouseOver Resume, onMouseOut Default ] [ text "Resume" ] ]
+              [ p [] [ a [ onMouseEnter Programming
+                         , onMouseOut Default
+                         , classList [ ("programming", True) ]
+                         ]
+                         [ text "Programming" ]
+                      ]
+              , p [] [ a [ onMouseOver Photography
+                         , onMouseOut Default
+                         , classList [ ("photography", True) ]
+                         ]
+                         [ text "Photography" ]
+                     ]
+              , p [] [ a [ onMouseOver Blog
+                         , onMouseOut Default
+                         , classList [ ("blog", True) ]
+                         ]
+                         [ text "Blog" ]
+                     ]
+              , p [] [ a [ onMouseOver Resume
+                         , onMouseOut Default
+                         , classList [ ("resume", True) ]
+                         ]
+                         [ text "Resume" ]
+                     ]
               ]
         ]
 
